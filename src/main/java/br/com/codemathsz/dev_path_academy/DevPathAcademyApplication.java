3be9a26f147958b7d2d60bb0f9abc63fe60d97e6
codemathsz <mo284891@gmail.com>
2025-01-13 13:45:16 -0300

    feat: initial commit

---
+++ src/main/java/br/com/codemathsz/dev_path_academy/DevPathAcademyApplication.java
@@ -0,0 +1,13 @@
+package br.com.codemathsz.dev_path_academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevPathAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevPathAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/codemathsz/dev_path_academy/DevPathAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codemathsz.dev_path_academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevPathAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
